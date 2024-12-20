06ff5dc17e4b2c5abc022a1b8374c4ad74185076
MKB-NEL <mkbertrand123@gmail.com>
2024-12-20 06:33:33 +0200

    Delete javascript.js

--- javascript.js
+++
@@ -1,8 +0,0 @@
-window.onscroll = function() {
-  var header = document.querySelector("header");
-  if (window.scrollY > 50) {
-    header.classList.add("shrink");
-  } else {
-    header.classList.remove("shrink");
-  }
-};
